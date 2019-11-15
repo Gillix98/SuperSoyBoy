7a0d877095df817069d0cc30a83046fe7c65c102
Gavin Crews <gavin98@live.ca>
2019-11-15 14:36:43 -0500

    finished adding hazards

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.transform.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+
+            Instantiate(playerDeathPrefab, coll.contacts[0].point, Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+            Destroy(coll.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
