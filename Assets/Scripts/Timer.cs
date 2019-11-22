8726f646db61f301cff51e74ea5e3e8923ac95a7
Gavin Crews [Student] <gavin.crews@Student.SL.On.Ca>
2019-11-22 13:51:21 -0500

    finished adding timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Timer : MonoBehaviour
 {
+    private Text timerText;
+
+    private void Awake()
+    {
+        timerText = GetComponent<Text>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,6 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timerText.text = System.Math.Round((decimal)Time.timeSinceLevelLoad, 2).ToString();
     }
 }
